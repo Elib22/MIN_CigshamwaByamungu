a8c554cf42f1309a555d96fb6bd5324227e051f4
Cigashamwa byamungu <ebyamungu28@gmail.com>
2020-07-28 16:23:01 +0000

    Everything

--- main.py
+++ main.py
@@ -2,14 +2,21 @@
 # each player may take a step a head
 # First one to 20 wins 
 
+Score = 0
 
 def function(k):
   L1 = int(input("WILL YOU GO FIRST OR SECOND ?*"))
   if k ==  "first" :
     print(" Player 1 starts ")
 
-def function2(k):
+def function2(l):
   L2 = input(" Enter 1 or 2 ")
-  if k == '1':
+  if l == '1':
+   result = 1 + Score
+   print(result)
 
-k = 
+function()
+function2()
+
+
+
